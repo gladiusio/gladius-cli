aca7318fb49968c0bb8a870b71157702c5ea8a43
Alex Godwin <>
2018-03-08 12:42:12 -0500

    First commit.

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# gladius-cli
-Gladius CLI for managing nodes
+# Gladius CLI
+
+Command line interface to control the node daemon.
+
+**This is a work in progress, not all of the below methods will work**
+
+## Installation
+
+Run the following command to install the Gladius CLI
+
+`npm install -g gladius-cli`
+
+And then install the daemon using either [docker](https://gladius.io) or
+[directly](https://gladius.io)
+
+# Setup
+
+Set up a static IP for the machine you will be running the Gladius node on, and
+forward port 443 and 80 on your router to this machine.
+
+_TODO: Talk about configuring a hostname for SSL in future_
+
+Run `gladius-node init` to initialize your Gladius node. Fill out the requested
+information and make sure you use the same private key as you used when you
+registered for the beta. This will ensure that you can sign up without a hitch.
+
+Once you have filled out your information (and there are no errors) you can run
+the command `gladius-node join-pool` to inform the Gladius daemon that this
+information is correct and you would like to join the beta pool. Your data will
+then be encrypted using the pool's public key, and put on the Ethereum
+blockchain. This may take some time to complete, so check your status with the
+command `gladius-node check-join` this can return a few different states
+(pending, denied, or accepted).
+
+Once you have been accepted to the pool, you can run
+the command `gladius-node start` to inform the Gladius daemon (it must be
+running and installed) that you would like to start accepting requests for
+content.
+
+To inform the daemon that you would like to stop accepting requests, run
+`gladius-node stop`. This will leave the daemon running, but will stop
+networking and transactions from taking place.
+
+# Configuration
+To configure features of the Gladius node software that would normally not be
+used by the average user, run `gladius-node config-location` to get the location
+of the "config.json" file that dictates parameters like ports and the IP address
+of the daemon.   
+
+# Notes and warnings
+You can see a full list of the commands available (some may not be fully
+  functional) by running `gladius-node --help`
+
+
+**Warning!** This is the beta implementation of the Gladius node, and as such it
+means there are some differences between how this functions and how future
+versions will. **Do not add private keys here that store more Ether or GLA (or
+any other cryptocurrency) than you are willing to lose.**
+
+In this version there is no way to specify the pool you would
+like to join, as the only pool available is run by Gladius. This version may
+also be less optimized than future versions, so run this on machines that you
+don't normally demand too much from.
---
+++ index.js
@@ -0,0 +1,162 @@
+#!/usr/bin/env node
+
+/*
+Gladius node CLI. Install with "npm install gladius-cli"
+then run: "gladius-node init" and follow the steps. After you have
+successfully completed this you can run: "gladius-node start"
+
+Requires the Gladius daemon to be installed and running to opperate.
+*/
+
+var prompt = require("prompt");
+var colors = require("colors/safe");
+var fs = require("fs");
+var path = require("path");
+
+var config = require("./config.js") // Load our config file
+
+var appDir = path.dirname(require.main.filename); // Get where this file is
+
+
+console.log(appDir);
+
+prompt.message = colors.blue("[Gladius-Node]");
+prompt.delimiter = colors.green(" ");
+
+prompt.start();
+
+
+// The functions below are called by the various options provided
+/******************************************************************************/
+
+// Prompt the user for information about themselves
+function init() {
+  // Create a schema for the paremeters to be asked
+  let schema = {
+    properties: {
+      email: {
+        description: "What's your email?",
+        pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        message: "Not a valid email",
+        required: true
+      },
+      name: {
+        description: "What's your first name?",
+        required: true
+      },
+      privateKeyLocation: {
+        description: "Private key location (my/key/here):",
+        required: true
+      }
+    }
+  };
+
+  // Prompt and forward data
+  prompt.get(schema, function(err, result) {
+    // TODO: Get the data like result.name result.email and forward using REST
+  });
+}
+
+// Build the help menu from the options
+function help(options) {
+  console.log("\n------Available arguments------ \n" + Object.keys(options).map(function(key) {
+    return ("\n\n" + colors.blue(key) + ": " + options[key].description);
+  }).join(""));
+}
+
+// Inform the control daemon that the node is ready
+function start() {
+
+}
+
+// Inform the control daemon that the node is no longer ready
+function stop() {
+
+}
+
+// Get the current status of the node daemons
+function status() {
+
+}
+
+// Join the beta pool
+function joinBetaPool() {
+
+}
+
+// List all available pools
+function listPools() {
+
+}
+
+// Check the status of current applications
+function checkJoin() {
+
+}
+
+/******************************************************************************/
+
+// Create options for the user
+var options = {
+  "init": {
+    description: "Gathers information about the user as well as configuration data.",
+    toCall: init
+  },
+  "start": {
+    description: "Start the Gladius node, and inform the pool of this.",
+    toCall: start
+  },
+  "stop": {
+    description: "Stop the Gladius node, and inform the pool of this.",
+    toCall: stop
+  },
+  "status": {
+    description: "Get's the current status of the node daemons",
+    toCall: status
+  },
+  "list-pools": {
+    description: "List all available pools",
+    toCall: listPools
+  },
+  "join-pool": {
+    description: "Join the beta pool (will have arguments in future to specify pool to join)",
+    toCall: joinBetaPool // Eventually replace with arbitrary pool upon launch
+  },
+  "check-join": {
+    description: "Check the status of your applications.",
+    toCall: checkJoin
+  },
+  "config-location": {
+    description: "Returns the location of the config.js file",
+    toCall: function() {
+      console.log()
+    }
+  },
+  "--help": {
+    description: "Show this menu",
+    toCall: function() {
+      help(options);
+    }
+  }
+}
+
+// Check the up status of the daemon
+function checkDaemon() {
+  // Check the status of the daemon from the status method
+
+  return true;
+}
+
+// Get the argument that the user provided
+var argument = process.argv[2];
+
+// Run the CLI
+if (checkDaemon()) {
+  if (argument in options) {
+    options[argument].toCall();
+  } else {
+    console.log("Unknown (or no) argument, please use --help to see available arguments")
+  }
+} else {
+  console.log(colors.red("Cannot connect to the Gladius daemon. See setup instructions here: https://github.com/gladiusio"));;
+}
