1c5e3b465f62ee73c339a90a54eb0886116b36c1
Celo44 <marcelomcandrew@gmail.com>
2018-03-16 16:34:06 -0400

    added comments

--- index.js
+++ index.js
@@ -25,197 +25,15 @@ prompt.delimiter = " ";
 
 prompt.start();
 
-
-// Check on init.json file
+// Check if init.json exists, if not create a blank one
 if (!fs.existsSync(appDir + "/init.json")) {
   reset();
 }
 
-// The functions below are called by the various options provided
-/******************************************************************************/
-
-// Prompt the user for information about themselves
-function init() {
-  // Create a schema for the paremeters to be asked
-  let schema = {
-    properties: {
-      email: {
-        description: "What's your email? (So we can contact you about the beta):",
-        pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        message: "Not a valid email",
-        required: true
-      },
-      name: {
-        description: "What's your first name?",
-        required: true
-      },
-      bio: {
-        description: "Short bio about why you're interested in Gladius:",
-        required: true
-      },
-      key: {
-        description: "Private key location (my/key/here):",
-        required: true,
-        message: "Not a valid key or key location",
-        conform: function(value) {
-          return (value == "test");
-        }
-      }
-    }
-  };
-
-  // Prompt and store the data
-  prompt.get(schema, function(err, result) {
-    console.log(colors.blue(
-      "\nIf the above information isn't correct, run init again. If it is, you can run gladius-node join-pool."
-    ));
-    initData = {
-      email: result.email,
-      name: result.name,
-      bio: result.bio,
-      key: result.key,
-      initialized: true // Set our initialized flag
-    };
-
-    writeInitInfo(initData); // Write it to a file
-  });
-}
-
-// Build the help menu from the options
-function help(options) {
-  console.log(colors.blue(
-      "\n--------------Available arguments-------------- \n") +
-    Object.keys(options).map(
-      function(key) {
-        return ("\n\n" + colors.blue(key) + ": " + options[key].description);
-      }
-    ).join(""));
-}
-
-// Inform the control daemon that the node is ready
-function start() {
-  axios.put(daemonAddress + "/api/status/", {
-      status: true
-    })
-    .then(function(response) {
-      console.log(response);
-    })
-    .catch(function(error) {
-      console.log(colors.red(
-        "Woah an error! Make sure your daemon is running and can be connected to"
-      ));
-      console.log(error);
-    });
-}
-
-// Inform the control daemon that the node is no longer ready
-function stop() {
-  axios.put(daemonAddress + "/api/status/", {
-      status: false
-    })
-    .then(function(response) {
-      console.log(response);
-    })
-    .catch(function(error) {
-      console.log(colors.red(
-        "Woah an error! Make sure your daemon is running and can be connected to"
-      ));
-      console.log(error);
-    });
-}
-
-// Get the current status of the node daemons
-function status() {
-  axios.get(daemonAddress + "/api/status/")
-    .then(function(response) {
-      console.log(response);
-    })
-    .catch(function(error) {
-      console.log(colors.red(
-        "Woah an error! Make sure your daemon is running and can be connected to"
-      ));
-      console.log(error);
-    });
-}
-
-// Join the beta pool
-function joinBetaPool() {
-  var initInfo = getInitInfo(); // Grab the information from initialization
-
-  if (initInfo.initialized) {
-    axios.post(daemonAddress + "/api/pools/beta", {
-        status: false
-      })
-      .then(function(response) {
-        console.log(response);
-      })
-      .catch(function(error) {
-        console.log(colors.red(
-          "Woah an error! Make sure your daemon is running and can be connected to"
-        ));
-        console.log(error);
-      });
-  } else {
-    console.log(colors.red(
-      "Error: You need to initialize your node first. Run gladius-node init to do this."
-    ))
-  }
-}
-
-// List all available pools
-function listPools() {
-  axios.get(daemonAddress + "/api/pools/")
-    .then(function(response) {
-      console.log(response);
-    })
-    .catch(function(error) {
-      console.log(colors.red(
-        "Woah an error! Make sure your daemon is running and can be connected to"
-      ));
-      console.log(error);
-    });
-}
-
-// Check the status of current applications
-function checkJoin() {
-  axios.get(daemonAddress + "/api/pools/check/beta")
-    .then(function(response) {
-      console.log(response);
-    })
-    .catch(function(error) {
-      console.log(colors.red(
-        "Woah an error! Make sure your daemon is running and can be connected to"
-      ));
-      console.log(error);
-    });
-}
-
-// Reset the init.json file for testing or for problem installations.
-function reset() {
-  var json = JSON.stringify({
-    email: "",
-    name: "",
-    bio: "",
-    key: "",
-    initialized: false
-  });
-  fs.writeFileSync(appDir + "/init.json", json);
-}
-
-/******************************************************************************/
-function writeInitInfo(info) {
-  var json = JSON.stringify(info);
-  fs.writeFileSync(appDir + "/init.json", json);
-}
-
-
-function getInitInfo() {
-  return JSON.parse(fs.readFileSync(appDir + "/init.json"));
-}
-
-
-// Create options for the user where description is the description of the
-// argument and toCall is a function.
+/**
+* Commands for the user to call
+* toCall is the function associated with the command
+*/
 var options = {
   "init": {
     description: "Gathers information about the user as well as configuration data.",
@@ -266,14 +84,220 @@ var options = {
   }
 }
 
-// Check the up status of the daemon
+/**
+* Onboarding, prompts users for information
+* creates a JSON file that stores the users info
+*/
+function init() {
+  // Create a schema for the prompts
+  let schema = {
+    properties: {
+      email: {
+        description: "What's your email? (So we can contact you about the beta):",
+        pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        message: "Not a valid email",
+        required: true
+      },
+      name: {
+        description: "What's your first name?",
+        required: true
+      },
+      bio: {
+        description: "Short bio about why you're interested in Gladius:",
+        required: true
+      },
+      key: {
+        description: "Private key location (my/key/here):",
+        required: true,
+        message: "Not a valid key or key location",
+        conform: function(value) {
+          return (value == "test");
+        }
+      }
+    }
+  };
+
+  // Prompt and store the data
+  prompt.get(schema, function(err, result) {
+    console.log(colors.blue(
+      "\nIf the above information isn't correct, run init again. If it is, you can run gladius-node join-pool."
+    ));
+    initData = {
+      email: result.email,
+      name: result.name,
+      bio: result.bio,
+      key: result.key,
+      initialized: true // Set our initialized flag
+    };
+
+    writeInitInfo(initData); // Write it to a file
+  });
+}
+
+/**
+* Builds and prints help menu
+* @param options - commands that the users run
+*/
+function help(options) {
+  console.log(colors.blue(
+      "\n--------------Available arguments-------------- \n") +
+    Object.keys(options).map(
+      function(key) {
+        return ("\n\n" + colors.blue(key) + ": " + options[key].description);
+      }
+    ).join(""));
+}
+
+/**
+* Start node daemon
+*/
+function start() {
+  axios.put(daemonAddress + "/api/status/", {
+      status: true
+    })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(colors.red(
+        "Woah an error! Make sure your daemon is running and can be connected to"
+      ));
+      console.log(error);
+    });
+}
+
+/**
+* Stop node daemon
+*/
+function stop() {
+  axios.put(daemonAddress + "/api/status/", {
+      status: false
+    })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(colors.red(
+        "Woah an error! Make sure your daemon is running and can be connected to"
+      ));
+      console.log(error);
+    });
+}
+
+/**
+* Check status of the node daemon
+*/
+function status() {
+  axios.get(daemonAddress + "/api/status/")
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(colors.red(
+        "Woah an error! Make sure your daemon is running and can be connected to"
+      ));
+      console.log(error);
+    });
+}
+
+/** WIP - should be changed to just join pool
+* See status of the node daemon
+*/
+function joinBetaPool() {
+  var initInfo = getInitInfo(); // Grab the information from initialization
+
+  if (initInfo.initialized) {
+    axios.post(daemonAddress + "/api/pools/beta", {
+        status: false
+      })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(colors.red(
+          "Woah an error! Make sure your daemon is running and can be connected to"
+        ));
+        console.log(error);
+      });
+  } else {
+    console.log(colors.red(
+      "Error: You need to initialize your node first. Run gladius-node init to do this."
+    ))
+  }
+}
+
+/**
+* List pools
+*/
+function listPools() {
+  axios.get(daemonAddress + "/api/pools/")
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(colors.red(
+        "Woah an error! Make sure your daemon is running and can be connected to"
+      ));
+      console.log(error);
+    });
+}
+
+/**
+* check the status of your application
+*/
+function checkJoin() {
+  axios.get(daemonAddress + "/api/pools/check/beta")
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(colors.red(
+        "Woah an error! Make sure your daemon is running and can be connected to"
+      ));
+      console.log(error);
+    });
+}
+
+/**
+* resets the init.json file to blank
+*/
+function reset() {
+  var json = JSON.stringify({
+    email: "",
+    name: "",
+    bio: "",
+    key: "",
+    initialized: false
+  });
+  fs.writeFileSync(appDir + "/init.json", json);
+}
+
+/**
+* write user data to init.json
+* @param info information to save
+*/
+function writeInitInfo(info) {
+  var json = JSON.stringify(info);
+  fs.writeFileSync(appDir + "/init.json", json);
+}
+
+/**
+* parse the user data from init.json
+*/
+function getInitInfo() {
+  return JSON.parse(fs.readFileSync(appDir + "/init.json"));
+}
+
+/**
+* Check the status of the daemon
+*/
 function checkDaemon() {
   // TODO: Check the status of the daemon from the status method
 
   return true;
 }
 
-// Get the argument that the user provided
+// Get the argument after "gladius-node"
 var argument = process.argv[2];
 
 
