6126cd2caf9e147808a5d051236663a1eed8fd1d
Celo44 <marcelomcandrew@gmail.com>
2018-03-19 15:14:18 -0400

    added config file support

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Configuration for the Gladius CLI
 
 var config = {
-  controlDaemonPort: 5544, // Port to access control daemon
-  controlDaemonAddress: "http://localhost", // What address to access the daemon
+  controlDaemonPort: 9545, // Port to access control daemon
+  controlDaemonAddress: "http://127.0.0.1", // What address to access the daemon
   transferKeyOverHttp: true // Send file path or send actual key (used for docker)
 };
 
--- index.js
+++ index.js
@@ -13,11 +13,12 @@ var colors = require("colors/safe");
 var fs = require("fs");
 var path = require("path");
 var axios = require("axios");
-
 var config = require("./config.js") // Load our config file
 
-var appDir = path.dirname(require.main.filename); // Get where this file is
-var daemonAddress = config.controlDaemonAddress + ":" + config.controlDaemonPort;
+let appDir = path.dirname(require.main.filename); // Get where this file is
+let daemonAddress = config.controlDaemonAddress + ":" + config.controlDaemonPort;
+let userData;
+let envData = JSON.parse(fs.readFileSync(appDir + "/env_config.json"));
 
 // Set up prompt
 prompt.message = colors.blue("[Gladius-Node]");
@@ -31,8 +32,59 @@ if (!fs.existsSync(appDir + "/init.json")) {
   reset();
 }
 
-// The functions below are called by the various options provided
-/******************************************************************************/
+/**
+* Commands for the user to call
+* toCall is the function associated with the command
+*/
+var options = {
+  "init": {
+    description: "Gathers information about the user as well as configuration data.",
+    toCall: init
+  },
+  "start": {
+    description: "Start the Gladius node, and inform the pool of this.",
+    toCall: start
+  },
+  "stop": {
+    description: "Stop the Gladius node, and inform the pool of this.",
+    toCall: stop
+  },
+  "status": {
+    description: "Get's the current status of the node daemons",
+    toCall: status
+  },
+  "list-pools": {
+    description: "List all available pools from the marketplace",
+    toCall: listPools
+  },
+  "join-pool": {
+    description: "Join the beta pool (will have arguments in future to specify pool to join)",
+    toCall: joinPool // Eventually replace with arbitrary pool upon launch
+  },
+  "check-join": {
+    description: "Check the status of your applications.",
+    toCall: checkJoin
+  },
+  "config-location": {
+    description: "Returns the location of the config.js file",
+    toCall: function() {
+      console.log(appDir + "/config.js")
+    }
+  },
+  "reset-init": {
+    description: "Resets init file (for testing or problem installations)",
+    toCall: function() {
+      reset();
+      console.log(colors.blue("Reset init.json"));
+    }
+  },
+  "--help": {
+    description: "Show this menu",
+    toCall: function() {
+      help(options);
+    }
+  }
+}
 
 // Prompt the user for information about themselves
 function init() {
@@ -53,12 +105,20 @@ function init() {
         description: "Short bio about why you're interested in Gladius:",
         required: true
       },
-      key: {
-        description: "Private key location (my/key/here):",
+      pvKey: {
+        description: "Wallet private key location (my/key/here):",
         required: true,
         message: "Not a valid key or key location",
         conform: function(value) {
-          return (value == "test");
+          return (value == "test"); //replace this to check if valid file path
+        }
+      },
+      pgpKey: {
+        description: "PGP key location (my/key/here):",
+        required: true,
+        message: "Not a valid key or key location",
+        conform: function(value) {
+          return (value == "test"); //replace this to check if valid file path
         }
       }
     }
@@ -69,19 +129,55 @@ function init() {
     console.log(colors.blue(
       "\nIf the above information isn't correct, run init again. If it is, you can run gladius-node join-pool."
     ));
-    initData = {
+    userData = {
       email: result.email,
       name: result.name,
       bio: result.bio,
-      key: result.key,
+      pvKey: result.pvKey,
+      pgpKey: result.pgpKey,
       initialized: true // Set our initialized flag
     };
-
-    writeInitInfo(initData); // Write it to a file
+    writeInitInfo(userData); // Write it to a file
   });
 }
 
-// Build the help menu from the options
+function createNode() {
+  axios.post(daemonAddress + "/api/node/create", {
+    name: userData.name,
+    email: userData.email,
+    status: "active"
+  })
+}
+
+/** WIP - should be changed to just join pool
+* See status of the node daemon
+*/
+function joinPool() {
+  var initInfo = getInitInfo(); // Grab the information from initialization
+
+  if (initInfo.initialized) {
+    axios.post(daemonAddress + "/api/pools/beta", {
+        status: false
+      })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(colors.red(
+          "Woah an error! Make sure your daemon is running and can be connected to"
+        ));
+        console.log(error);
+      });
+  } else {
+    console.log(colors.red(
+      "Error: You need to initialize your node first. Run gladius-node init to do this."
+    ))
+  }
+}
+/**
+* Builds and prints help menu
+* @param options - commands that the users run
+*/
 function help(options) {
   console.log(colors.blue(
       "\n--------------Available arguments-------------- \n") +
@@ -92,7 +188,9 @@ function help(options) {
     ).join(""));
 }
 
-// Inform the control daemon that the node is ready
+/**
+* Start accepting connections
+*/
 function start() {
   axios.put(daemonAddress + "/api/status/", {
       status: true
@@ -108,7 +206,9 @@ function start() {
     });
 }
 
-// Inform the control daemon that the node is no longer ready
+/**
+* Stop accepting connections
+*/
 function stop() {
   axios.put(daemonAddress + "/api/status/", {
       status: false
@@ -138,31 +238,9 @@ function status() {
     });
 }
 
-// Join the beta pool
-function joinBetaPool() {
-  var initInfo = getInitInfo(); // Grab the information from initialization
-
-  if (initInfo.initialized) {
-    axios.post(daemonAddress + "/api/pools/beta", {
-        status: false
-      })
-      .then(function(response) {
-        console.log(response);
-      })
-      .catch(function(error) {
-        console.log(colors.red(
-          "Woah an error! Make sure your daemon is running and can be connected to"
-        ));
-        console.log(error);
-      });
-  } else {
-    console.log(colors.red(
-      "Error: You need to initialize your node first. Run gladius-node init to do this."
-    ))
-  }
-}
-
-// List all available pools
+/**
+* List pools
+*/
 function listPools() {
   axios.get(daemonAddress + "/api/pools/")
     .then(function(response) {
@@ -202,7 +280,6 @@ function reset() {
   fs.writeFileSync(appDir + "/init.json", json);
 }
 
-/******************************************************************************/
 function writeInitInfo(info) {
   var json = JSON.stringify(info);
   fs.writeFileSync(appDir + "/init.json", json);
