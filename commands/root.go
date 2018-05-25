7d44dd4b9a91d9df52fb1d3e796daf2223613110
Marcelo McAndrew <marcelomcandrew@gmail.com>
2018-05-25 12:11:19 -0400

    hotfix

--- commands/root.go
+++ commands/root.go
@@ -16,6 +16,7 @@ var rootCmd = &cobra.Command{
 		fmt.Println("\nWelcome to the Gladius CLI!")
 		fmt.Println("\nHere are the commands to create a node and apply to a pool in order:")
 		fmt.Println("\n$ gladius create")
+		fmt.Println("$ gladius apply")
 		fmt.Println("$ gladius check")
 		fmt.Println("\nAfter you are accepted into a pool, you can become an edge node:")
 		fmt.Println("\n$ gladius node start")
